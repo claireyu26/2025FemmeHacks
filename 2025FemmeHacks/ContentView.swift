ed71adceb6b917f0f84481c091faa099aca8a088
claireyu26 <clairesunyu@gmail.com>
2025-02-07 16:01:50 -0500

    Initial Commit

---
+++ 2025FemmeHacks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2025FemmeHacks
+//
+//  Created by Student on 2/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2025FemmeHacks/_025FemmeHacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  _025FemmeHacksApp.swift
+//  2025FemmeHacks
+//
+//  Created by Student on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct _025FemmeHacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
